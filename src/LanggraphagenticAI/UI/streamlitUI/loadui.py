588695d290f84056ba5616a5a622725477c6eee2
Shishupalreddyr-alt <rshishupalr@gmail.com>
2025-07-06 13:55:26 +0530

    Load UI configuration and folder structure

---
+++ src/LanggraphagenticAI/UI/streamlitUI/loadui.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import os
+from datetime import date
+
+from langchain_core.messages import AIMessage,HumanMessage
+
+from src.LanggraphagenticAI.UI.uiconfig import Config     
+
+class LoadStreamlitUI:
+    def __init__(self):
+        self.config = Config()
+        self.user_controls={}
